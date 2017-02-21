b82505e23112cdd5e7bd28169d56045229f19fe3
wzmli <wzmichael.li@gmail.com>
2017-02-21 17:05:06 -0500

    need a function to compute the characteristic matrix

--- clmmtest.R
+++ clmmtest.R
@@ -9,7 +9,7 @@ library(lme4)
 set.seed(2830)
 l <- 400
 
-y <- sample(1:2, l, prob=1:2, replace=TRUE)
+y <- sample(1:3, l, prob=1:3, replace=TRUE)
 x <- rnorm(l)
 religion <- sample(1:3, l, prob=1:3, replace=TRUE)
 country <- sample(1:3, l, prob=1:3, replace=TRUE)
@@ -24,31 +24,24 @@ dat <- data.frame(x=x, y=y
 )
 ## Naive lmer works, default behaviour is like our "base"
 formula <- as.factor(y)~x+country+religion+(1|village)
-summary(clmm2(formula, data=dat))
+summary(clmm(formula, data=dat))
 
 fillbase <- clmmFill(formula,data=dat,NArows = list(dat$country==3), fillvar=list("religion"),Fillmethod="base")
-mmbase <- clmmMM(formula,data=dat,NArows = list(dat$country==3), fillvar=list("religion"),Fillmethod="base")
+mmbase <- CMM(formula,data=dat,NArows = list(dat$country==3), fillvar=list("religion"),Fillmethod="base")
+CMbase <- mmbase[,c(1:6)]
 fillmean <- clmmFill(formula,data=dat,NArows = list(dat$country==3), fillvar=list("religion"),Fillmethod="mean")
-mmmean <- clmmMM(formula,data=dat,NArows = list(dat$country==3), fillvar=list("religion"),Fillmethod="mean")
-
-fillbase
-fillmean
+mmmean <- CMM(formula,data=dat,NArows = list(dat$country==3), fillvar=list("religion"),Fillmethod="mean")
+CMmean <- mmmean[,c(1:6)]
 
 fillbase$coefficients
 fillmean$coefficients
 
-dat <- droplevels(within(dat, {
-  religion[country==3] <- NA
-  
-}))
-
-CM <- CMFill(y~x+country+religion, dat,rowfill=4, colfill=c(5,6))
-bhat <- CM %*% fillbase$coefficients
-print(bhat)
-print(fillmean$coefficients)
-
-bhat_mike <- solve(t(mmmean)%*%mmmean)%*%t(mmmean)%*%mmbase%*%fillbase$coefficients
-
+checkS <- solve(t(CMmean)%*%CMmean)%*%t(CMmean)%*%CMbase
+print(checkS)
+SS <- matrix(data=0,nrow=7,ncol=7)
+SS[1,1] <- 1
+SS[2:7,2:7] <- checkS
+bhat <- SS%*%fillbase$coefficients
 
 # 
 # 
